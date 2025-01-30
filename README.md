e2db26955ffb12a18b9d31ba5e87a9258a747203
valdimario <maiavaldimario@gmail.com>
2025-01-30 14:19:31 -0300

    Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente.

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Olá, meu nome é Valdimario e, neste documento, vou falar sobre o meu primeiro projeto.
+
+Esta aplicação tem como objetivo permitir que os usuários insiram nomes de amigos em uma lista para, posteriormente, realizar um sorteio aleatório e determinar quem será o "amigo secreto".
+
+O funcionamento é simples: o usuário poderá adicionar nomes através de um campo de texto e de um botão "Adicionar". À medida que os nomes forem inseridos, eles serão exibidos em uma lista visível na página.
+
+Ao finalizar a inclusão dos participantes, um botão "Sortear Amigo" permitirá a seleção aleatória de um dos nomes, exibindo o resultado na tela.
--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; //criamos uma lista para armazenar os nomes dos amigos
+
+function adicionarAmigo(){
+ let nomeInserido = document.querySelector('input').value; //value significa que vamos armazenar dentro da variável nomeInserido o que o usuário digitar na caixa de texto
+        console.log(nomeInserido); 
+    
+    if(nomeInserido == ''){
+        return alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(nomeInserido); // .push() é usado para adicionar um elemento ao final de uma lista
+        console.log(amigos);
+        limparCampo() 
+    }
+    
+}
+
+function limparCampo(){
+    nomeInserido = document.querySelector('input'); // retorna ao input no HTML e armazena ele na variável nomeInserido
+    nomeInserido.value = ''; // define o campo input em uma string vazia
+}
